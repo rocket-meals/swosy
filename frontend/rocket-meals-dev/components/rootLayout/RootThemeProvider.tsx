9dbcd3a514a45ea67115e4fcc61081854f8d35a1
Nils <nilsbaumgartner1994@gmail.com>
2024-01-23 17:18:54 +0100

    Commented Color contrast

--- frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
+++ frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
@@ -2,6 +2,7 @@ import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
 import React from 'react';
 import {Appearance} from "react-native";
 import {useMyColorSchemeNameDetermined, useThemeDetermined} from "@/helper/sync_state_helper/custom_sync_states/ColorScheme";
+import {MyGlobalActionSheet} from "@/components/actionsheet/MyGlobalActionSheet";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -18,6 +19,7 @@ export const RootThemeProvider = (props: RootThemeProviderProps) => {
       <ThemeProvider value={theme}>
             {/* Render the children */}
             {props?.children}
+            <MyGlobalActionSheet />
       </ThemeProvider>
   )
 }
