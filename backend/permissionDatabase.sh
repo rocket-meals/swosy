625a4c8b9fe24d80b1f3b10e3852ac4ed1f027b5
Nils <nilsbaumgartner1994@gmail.com>
2024-02-16 18:13:37 +0100

    Set permission also for api-translation-key

--- backend/permissionDatabase.sh
+++ backend/permissionDatabase.sh
@@ -4,4 +4,6 @@ chmod -R 777 ./Backend/database/
 chmod -R 777 ./Backend/uploads/
 touch ./Backend/database/data.db
 chmod 777 ./Backend/database/data.db
+echo "Setting read/write permission for secrets"
+chmod -R 777 ./Backend/secrets/
 echo "Finished"
