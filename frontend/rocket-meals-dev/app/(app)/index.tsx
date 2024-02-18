81b94b51e8a5ac41726b092b8e446319a701f0fa
Nils <nilsbaumgartner1994@gmail.com>
2024-02-18 19:21:40 +0100

    Test weird android bug on expo go version

--- frontend/rocket-meals-dev/app/(app)/index.tsx
+++ frontend/rocket-meals-dev/app/(app)/index.tsx
@@ -1,20 +1,55 @@
-import {StyleSheet} from 'react-native';
 import {useFocusEffect, useRouter} from "expo-router";
+import {useState} from "react";
+import {MySafeAreaView} from "@/components/MySafeAreaView";
+import {MyScrollView} from "@/components/scrollview/MyScrollView";
+import {Heading, Text, View} from "@/components/Themed";
+import {MyButton} from "@/components/buttons/MyButton";
 
 export default function TabOneScreen() {
 
   const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  console.log("Rendering (app)/index.tsx")
 
   useFocusEffect(() => {
     // Call the replace method to redirect to a new route without adding to the history.
     // We do this in a useFocusEffect to ensure the redirect happens every time the screen
     // is focused.
-    router.push('/foodoffers')
-
-    // TODO: https://docs.expo.dev/router/reference/redirects/
-    // replace does not work on. Tested on web
-    // router.replace('/home')
+    try {
+        //router.push('/foodoffers/')
+    } catch (e: any) {
+        setError(e.message)
+    }
   });
 
-  return null;
+  return <MySafeAreaView>
+    <MyScrollView>
+        <View >
+            <Heading>{"Loading..."}</Heading>
+          <MyButton accessibilityLabel={"Retry /foodoffers/"} onPress={() => {
+            try {
+              router.push('/foodoffers/')
+            } catch (e: any) {
+              setError(e.message)
+            }
+          } } text={"Retry /foodoffers/"}/>
+          <MyButton accessibilityLabel={"Retry /home/"} onPress={() => {
+            try {
+              router.push('/home/')
+            } catch (e: any) {
+              setError(e.message)
+            }
+          } } text={"Retry /home/"}/>
+          <MyButton accessibilityLabel={"Retry /fail/"} onPress={() => {
+            try {
+              router.push('/fail/')
+            } catch (e: any) {
+              setError(e.message)
+            }
+          } } text={"Retry /fail/"}/>
+            <Text>{error}</Text>
+        </View>
+    </MyScrollView>
+  </MySafeAreaView>
 }
