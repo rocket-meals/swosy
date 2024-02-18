3cd49ab37440e1611f18432f7acda19130329791
Nils <nilsbaumgartner1994@gmail.com>
2024-02-18 19:40:01 +0100

    Fixed android routing bug. Do not use "useRouter".

--- frontend/rocket-meals-dev/app/(app)/home/index.tsx
+++ frontend/rocket-meals-dev/app/(app)/home/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>{"Home"}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
--- frontend/rocket-meals-dev/app/(app)/index.tsx
+++ frontend/rocket-meals-dev/app/(app)/index.tsx
@@ -1,14 +1,9 @@
-import {useFocusEffect, useRouter} from "expo-router";
-import {useState} from "react";
+import {useFocusEffect, router} from "expo-router";
 import {MySafeAreaView} from "@/components/MySafeAreaView";
 import {MyScrollView} from "@/components/scrollview/MyScrollView";
-import {Heading, Text, View} from "@/components/Themed";
-import {MyButton} from "@/components/buttons/MyButton";
+import {View} from "@/components/Themed";
 
 export default function TabOneScreen() {
-
-  const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
 
   console.log("Rendering (app)/index.tsx")
 
@@ -16,39 +11,13 @@ export default function TabOneScreen() {
     // Call the replace method to redirect to a new route without adding to the history.
     // We do this in a useFocusEffect to ensure the redirect happens every time the screen
     // is focused.
-    try {
-        //router.push('/foodoffers/')
-    } catch (e: any) {
-        setError(e.message)
-    }
+    router.push('/foodoffers/')
   });
 
   return <MySafeAreaView>
     <MyScrollView>
         <View >
-            <Heading>{"Loading..."}</Heading>
-          <MyButton accessibilityLabel={"Retry /foodoffers/"} onPress={() => {
-            try {
-              router.push('/foodoffers/')
-            } catch (e: any) {
-              setError(e.message)
-            }
-          } } text={"Retry /foodoffers/"}/>
-          <MyButton accessibilityLabel={"Retry /home/"} onPress={() => {
-            try {
-              router.push('/home/')
-            } catch (e: any) {
-              setError(e.message)
-            }
-          } } text={"Retry /home/"}/>
-          <MyButton accessibilityLabel={"Retry /fail/"} onPress={() => {
-            try {
-              router.push('/fail/')
-            } catch (e: any) {
-              setError(e.message)
-            }
-          } } text={"Retry /fail/"}/>
-            <Text>{error}</Text>
+
         </View>
     </MyScrollView>
   </MySafeAreaView>
