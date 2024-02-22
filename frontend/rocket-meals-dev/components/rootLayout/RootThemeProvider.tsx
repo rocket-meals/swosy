05b50e714102da1d895cfe30cfdd2f8dd5e7fc1f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2024-02-22 15:01:21 +0100

    Update StatusBar text color

--- frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
+++ frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
@@ -1,8 +1,9 @@
 import {ThemeProvider} from '@react-navigation/native';
 import React from 'react';
-import {useThemeDetermined} from "@/states/ColorScheme";
+import {useThemeDetermined, useIsDarkTheme} from "@/states/ColorScheme";
 import {MyGlobalActionSheet} from "@/components/actionsheet/MyGlobalActionSheet";
 import {RootFabHolder} from "@/components/rootLayout/RootFabHolder";
+import { StatusBar } from 'expo-status-bar';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -14,9 +15,13 @@ export interface RootThemeProviderProps {
 }
 export const RootThemeProvider = (props: RootThemeProviderProps) => {
     const theme = useThemeDetermined()
+    const isDarkTheme = useIsDarkTheme()
+
+    const statusbarTextColorStyle = isDarkTheme ? "light" : "dark"
 
   return(
       <ThemeProvider value={theme}>
+            <StatusBar style={statusbarTextColorStyle} />
             {/* Render the children */}
             {props?.children}
             <RootFabHolder />
