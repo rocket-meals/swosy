f1df7604678beee05269cc8731b51b35efb8ba6b
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2024-03-01 23:22:22 +0100

    Update index.tsx

--- frontend/rocket-meals-dev/app/(app)/course-timetable/index.tsx
+++ frontend/rocket-meals-dev/app/(app)/course-timetable/index.tsx
@@ -237,7 +237,7 @@ export default function CourseTimetableScreen() {
             )
         } else {
             return (
-                <View style={{width: "100%", height: "100%", backgroundColor: "red"}}>
+                <View style={{width: "100%", height: "100%"}}>
                     <CourseTimetableSchedule
                         key={amountOfDaysToShowOnScreen+"-"+firstDayOfWeek+"-"+startTime+"-"+endTime}
                         weekStartsOn={firstDayOfWeek}
