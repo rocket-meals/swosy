5b25f13c84953edcaa89b47eccc82b78ec50929c
Nils <nilsbaumgartner1994@gmail.com>
2024-03-14 09:27:29 +0100

    Added new Types from Backend

--- frontend/rocket-meals-dev/helper/database/databaseTypes/types.ts
+++ frontend/rocket-meals-dev/helper/database/databaseTypes/types.ts
@@ -36,6 +36,9 @@ export type AppSettings = {
   date_privacy_policy_updated?: string | null;
   date_updated?: string | null;
   foods_enabled?: boolean | null;
+  foods_feedbacks_comments_type?: string | null;
+  foods_feedbacks_custom_url?: string | null;
+  foods_feedbacks_labels_type?: string | null;
   foods_parsing_enabled?: boolean | null;
   foods_parsing_last_date?: string | null;
   foods_parsing_status?: string | null;
@@ -198,7 +201,6 @@ export type Canteens = {
   date_created?: string | null;
   date_updated?: string | null;
   external_identifier?: string | null;
-  foodoffers: any[] | Foodoffers[];
   id: string;
   sort?: number | null;
   status?: string | null;
@@ -731,6 +733,7 @@ export type FoodsFeedbacks = {
   date_updated?: string | null;
   food?: string | Foods | null;
   id: string;
+  labels: any[] | FoodsFeedbacksFoodsFeedbacksLabels[];
   notify?: boolean | null;
   profile?: string | Profiles | null;
   rating?: number | null;
@@ -738,6 +741,34 @@ export type FoodsFeedbacks = {
   status: string;
   user_created?: string | DirectusUsers | null;
   user_updated?: string | DirectusUsers | null;
+};
+
+export type FoodsFeedbacksFoodsFeedbacksLabels = {
+  foods_feedbacks_id?: string | FoodsFeedbacks | null;
+  foods_feedbacks_labels_id?: string | FoodsFeedbacksLabels | null;
+  id: number;
+};
+
+export type FoodsFeedbacksLabels = {
+  alias?: string | null;
+  date_created?: string | null;
+  date_updated?: string | null;
+  id: string;
+  sort?: number | null;
+  status: string;
+  translations: any[] | FoodsFeedbacksLabelsTranslations[];
+  user_created?: string | DirectusUsers | null;
+  user_updated?: string | DirectusUsers | null;
+};
+
+export type FoodsFeedbacksLabelsTranslations = {
+  be_source_for_translations?: boolean | null;
+  foods_feedbacks_labels_id?: string | FoodsFeedbacksLabels | null;
+  id: number;
+  languages_code?: string | Languages | null;
+  let_be_translated?: boolean | null;
+  text?: string | null;
+  translation_settings: string;
 };
 
 export type FoodsFeedbacksPredefinedFoodFeedbackLabels = {
@@ -1041,6 +1072,9 @@ export type CustomDirectusTypes = {
   foodoffers_markings: FoodoffersMarkings[];
   foods: Foods[];
   foods_feedbacks: FoodsFeedbacks[];
+  foods_feedbacks_foods_feedbacks_labels: FoodsFeedbacksFoodsFeedbacksLabels[];
+  foods_feedbacks_labels: FoodsFeedbacksLabels[];
+  foods_feedbacks_labels_translations: FoodsFeedbacksLabelsTranslations[];
   foods_feedbacks_predefined_food_feedback_labels: FoodsFeedbacksPredefinedFoodFeedbackLabels[];
   foods_markings: FoodsMarkings[];
   foods_translations: FoodsTranslations[];
