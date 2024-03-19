ab80b0c75e016dc0153ef76c9f2edbfd67d7fc77
Nils <nilsbaumgartner1994@gmail.com>
2024-03-19 14:54:15 +0100

    Fix Intl not available on Android

--- frontend/rocket-meals-dev/components/pricing/PricingBadge.tsx
+++ frontend/rocket-meals-dev/components/pricing/PricingBadge.tsx
@@ -7,13 +7,45 @@ export type PricingBadgeProps = {
   color?: string;
 }
 
+//https://helloacm.com/javascripts-tofixed-implementation-without-rounding/#:~:text=The%20idea%20is%20to%20convert,for%20exactly%20n%20decimal%20places.
+function toFixedNoRounding(number: number, fractions: number) {
+	const reg = new RegExp("^-?\\d+(?:\\.\\d{0," + fractions + "})?", "g")
+	const a = number.toString().match(reg)[0];
+	const dot = a.indexOf(".");
+	if (dot === -1) { // integer, insert decimal dot and pad up zeros
+		return a + "." + "0".repeat(fractions);
+	}
+	const b = fractions - (a.length - dot) + 1;
+	return b > 0 ? (a + "0".repeat(b)) : a;
+}
+
+function formatPrice(price: number | undefined | null): string | number {
+	let currency = "€";
+
+	//TODO? What about different currencies? Should we transform/calculate it?
+	if(price===undefined || price===null){
+		return "?"+" "+currency;
+	}
+	//Since Intl.NumberFormat does not work on android, we need to do it ourself
+	try{
+		let priceWithFraction = ""+toFixedNoRounding(price, 2);
+		priceWithFraction = priceWithFraction.replace(".", ",");
+		return priceWithFraction+" "+currency;
+	} catch (err){
+		//console.log(err);
+	}
+	return price;
+}
+
 export default function PricingBadge(props: PricingBadgeProps) {
 	const projectColor = props.color ?? useProjectColor();
 	const projectContrastColor = useMyContrastColor(projectColor);
 
-	const priceContent =new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-		props.price,
-	)
+	// Does not work on android
+	// new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
+	// 		props.price,
+	// 	)
+	const priceContent = formatPrice(props.price);
 
 
 	return (
