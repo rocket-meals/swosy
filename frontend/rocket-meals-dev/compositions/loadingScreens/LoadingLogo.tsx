6480462337fb134d0a80935635b30a79442f1a9b
Nils <nilsbaumgartner1994@gmail.com>
2024-03-24 18:44:22 +0100

    HotFix Lottie Bug - On change server

--- frontend/rocket-meals-dev/compositions/loadingScreens/LoadingLogo.tsx
+++ frontend/rocket-meals-dev/compositions/loadingScreens/LoadingLogo.tsx
@@ -12,8 +12,11 @@ export const LoadingLogo = ({...props}) => {
 	const projectLogoAssetId = useProjectLogoAssetId()
 
 	if (projectLogoAssetId) {
-		return <ProjectLogo style={{width: width, height: height}}/>
+		return <ProjectLogo key={projectLogoAssetId} style={{width: width, height: height}}/>
 	}
 
-	return <MyProjectColoredLottieAnimation source={rocketSource} accessibilityLabel={'Rocket'} style={{width: width, height: height}}/>;
+	// When switching server-configurations, the lottie animation is not destroyed properly
+	// Error: this.elements[i].destroy() is not a function
+	// https://github.com/airbnb/lottie-web/issues/2275
+	//return <MyProjectColoredLottieAnimation source={rocketSource} accessibilityLabel={'Rocket'} style={{width: width, height: height}}/>;
 }
