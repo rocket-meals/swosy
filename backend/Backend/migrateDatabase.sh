68e048489c2de8c8d5f25d15f7e190871b026a2d
Nils <nilsbaumgartner1994@gmail.com>
2024-04-16 11:26:44 +0200

    Removed Schema Custom Sync

--- backend/Backend/migrateDatabase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# check if the ./node_modules/ exists
-if [ ! -d "./node_modules/" ]; then
-  echo "Node modules not found, running npm install"
-  npm install
-fi
-echo "Running npm run schema-apply:latest to apply the latest schema"
-npm run schema-apply:latest
