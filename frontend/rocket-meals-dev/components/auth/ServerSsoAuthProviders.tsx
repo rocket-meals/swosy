f02203fafaef07504169c2ae3c34843f56c0bfba
Nils <nilsbaumgartner1994@gmail.com>
2024-04-30 09:42:47 +0200

    Closes #131 - sort apple and google sso

--- frontend/rocket-meals-dev/components/auth/ServerSsoAuthProviders.tsx
+++ frontend/rocket-meals-dev/components/auth/ServerSsoAuthProviders.tsx
@@ -4,6 +4,13 @@ import {ButtonAuthProviderCustom} from '@/components/buttons/ButtonAuthProviderC
 import {useEffect, useState} from 'react';
 import {AuthProvider, ServerAPI} from '@/helper/database/server/ServerAPI';
 import {View} from '@/components/Themed';
+import {PlatformHelper} from "@/helper/PlatformHelper";
+
+function isProviderNameEqualTo(provider: AuthProvider, name: string) {
+	const lowerCaseName = name.toLowerCase();
+	const lowerCaseProviderName = provider.name.toLowerCase();
+	return lowerCaseProviderName === lowerCaseName;
+}
 
 export const ServerSsoAuthProviders = () => {
 	const [authProviders, setAuthProviders] = useState<AuthProvider[] | null>(null);
@@ -13,6 +20,32 @@ export const ServerSsoAuthProviders = () => {
 		(async () => {
 			//console.log("ServerSsoAuthProviders useEffect")
 			const remoteAuthProviders = await ServerAPI.getAuthProviders();
+
+			// Sort Apple and Google to the top if the platform is iOS or Android
+			const isApple = PlatformHelper.isIOS();
+			const isGoogle = PlatformHelper.isAndroid();
+
+			const appleProviderName = 'apple';
+			const googleProviderName = 'google';
+
+			// switch apple and google positions depending on platform
+			const appleProvider = remoteAuthProviders.find(provider => isProviderNameEqualTo(provider, appleProviderName));
+			const googleProvider = remoteAuthProviders.find(provider => isProviderNameEqualTo(provider, googleProviderName));
+
+			if (appleProvider && googleProvider) {
+				const appleIndex = remoteAuthProviders.indexOf(appleProvider);
+				const googleIndex = remoteAuthProviders.indexOf(googleProvider);
+
+				if (isApple) {
+					remoteAuthProviders[appleIndex] = googleProvider;
+					remoteAuthProviders[googleIndex] = appleProvider;
+				} else if (isGoogle) {
+					remoteAuthProviders[appleIndex] = googleProvider;
+					remoteAuthProviders[googleIndex] = appleProvider;
+				}
+			}
+
+
 			//console.log("ServerSsoAuthProviders useEffect authProviders", remoteAuthProviders)
 			setAuthProviders(remoteAuthProviders);
 		})()
