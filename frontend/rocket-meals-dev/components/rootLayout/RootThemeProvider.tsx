0a8bff6177210ca1a552026c8633b7507f08a29d
Nils <nilsbaumgartner1994@gmail.com>
2024-05-05 16:53:54 +0200

    Fix Native Click Event Block

--- frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
+++ frontend/rocket-meals-dev/components/rootLayout/RootThemeProvider.tsx
@@ -41,76 +41,84 @@ export interface RootThemeProviderProps {
     children?: React.ReactNode;
 }
 
+const RootTextAndIconDimensions = () => {
+	const [textDimensions, setTextDimensions] = useSyncState(NonPersistentStore.textDimensions);
+	const [iconDimensions, setIconDimensions] = useSyncState(NonPersistentStore.iconDimensions);
+	const imageWidth = useIconWithInPixel(1);
+
+	return (
+		<View
+			pointerEvents="none" // Do not block touch events
+			style={{
+			position: 'absolute',
+			top: 0,
+			left: 0,
+//				width: 0, // has to be outcommented to get the width on iOS
+//				height: 0, // has to be outcommented to get the height on iOS
+			// hide the view
+			opacity: 0,
+		}}
+			  accessible={false} accessibilityElementsHidden={true}
+		>
+			<View style={{
+				backgroundColor: "red",
+				flexDirection: "row"
+			}}>
+				<Text onLayout={(event) => {
+					const {width, height} = event.nativeEvent.layout;
+					setTextDimensions((currentDimensions) => {
+						return {
+							width: width,
+							height: height
+						}
+					})
+				}}>{"M"}</Text>
+			</View>
+			<Text>{textDimensions?.width}</Text>
+			<View style={{
+				backgroundColor: "blue",
+				width: textDimensions?.width,
+				height: 10,
+			}} />
+			<View style={{
+				backgroundColor: "red",
+				flexDirection: "row"
+			}}>
+				<Icon name={IconNames.star_active_icon} onLayout={(event) => {
+					const {width, height} = event.nativeEvent.layout;
+					setIconDimensions((currentDimensions) => {
+						console.log("SetIconDimensions: "+width);
+						return {
+							width: width,
+							height: height
+						}
+					})
+				}} />
+			</View>
+			<Text>{iconDimensions?.width}</Text>
+			<Text>{imageWidth}</Text>
+			<View style={{
+				backgroundColor: "blue",
+				width: iconDimensions?.width,
+				height: 10,
+			}} />
+		</View>
+	)
+}
+
 const RootContent = (props: RootThemeProviderProps) => {
 	const [modalConfig, setModalConfig] = useModalGlobalContext();
 
 	const backgroundColor = useViewBackgroundColor();
 
-	const [textDimensions, setTextDimensions] = useSyncState(NonPersistentStore.textDimensions);
-	const [iconDimensions, setIconDimensions] = useSyncState(NonPersistentStore.iconDimensions);
-	const imageWidth = useIconWithInPixel(1);
-
 	const appIsAccessible = !modalConfig
 
 	return(
 		<>
+			<RootTextAndIconDimensions />
 			<View style={{height: '100%', width: '100%', backgroundColor: backgroundColor}} accessible={appIsAccessible} accessibilityElementsHidden={!appIsAccessible}>
 				{/* Render the children respecting the action sheet's visibility */}
 				{props.children}
-			</View>
-			<View style={{
-				position: 'absolute',
-				top: 0,
-				left: 0,
-//				width: 0, // has to be outcommented to get the width on iOS
-//				height: 0, // has to be outcommented to get the height on iOS
-				// hide the view
-				opacity: 0,
-			}}
-				  accessible={false} accessibilityElementsHidden={true}
-			>
-				<View style={{
-					backgroundColor: "red",
-					flexDirection: "row"
-				}}>
-					<Text onLayout={(event) => {
-						const {width, height} = event.nativeEvent.layout;
-						setTextDimensions((currentDimensions) => {
-							return {
-								width: width,
-								height: height
-							}
-						})
-					}}>{"M"}</Text>
-				</View>
-				<Text>{textDimensions?.width}</Text>
-				<View style={{
-					backgroundColor: "blue",
-					width: textDimensions?.width,
-					height: 10,
-				}} />
-				<View style={{
-					backgroundColor: "red",
-					flexDirection: "row"
-				}}>
-					<Icon name={IconNames.star_active_icon} onLayout={(event) => {
-						const {width, height} = event.nativeEvent.layout;
-						setIconDimensions((currentDimensions) => {
-							console.log("SetIconDimensions: "+width);
-							return {
-								width: width,
-								height: height
-							}
-						})
-					}} />
-				</View>
-				<Text>{iconDimensions?.width}</Text>
-				<Text>{imageWidth}</Text>
-				<View style={{
-					backgroundColor: "blue",
-					width: iconDimensions?.width,
-					height: 10,
-				}} />
 			</View>
 			<RootFabHolder />
 		</>
