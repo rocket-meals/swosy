01a7b3933f1d14b8ede1fb65f1597dc293fd2214
Nils <nilsbaumgartner1994@gmail.com>
2024-05-28 18:21:52 +0200

    Added first News Parse Tester for Hannover

---
+++ backend/Backend/directusExtensions/directus-extension-rocket-meals-bundle/src/news-sync-hook/__tests__/NewsTestHannover.ts
@@ -0,0 +1,14 @@
+// small jest test
+import {describe, expect, it} from '@jest/globals';
+import {StudentenwerkHannoverNews_Parser} from "../StudentenwerkHannoverNews_Parser";
+
+describe("NewsTestHannover", () => {
+    let newsParser = new StudentenwerkHannoverNews_Parser();
+
+    it("should find news with fields", async () => {
+        let limitAmountNews = 2;
+        let news = await newsParser.getRealNewsItems(limitAmountNews);
+        console.log("Found news: " + news.length);
+        expect(news.length).toBeGreaterThan(0);
+    });
+});
