35eacbe4dde782af1a9b420467c5c1ba7a0b94b7
Nils <nilsbaumgartner1994@gmail.com>
2024-06-12 02:37:30 +0200

    Added default for dev to studi futter

--- frontend/app/constants/ServerConfiguration.ts
+++ frontend/app/constants/ServerConfiguration.ts
@@ -1,7 +1,9 @@
 const DEFAULT_SERVER_URL = 'https://test.rocket-meals.de/rocket-meals/api';
 const LOCALHOST = 'http://127.0.0.1/rocket-meals/api';
-const isDev = false;
+const REAL_TEST_SERVER_URL = "https://studi-futter.rocket-meals.de/rocket-meals/api";
+const isDev = process.env.NODE_ENV === 'development';
+let DEV_SERVER_URL = REAL_TEST_SERVER_URL
 
 export default {
-	ServerUrl: isDev ? LOCALHOST : DEFAULT_SERVER_URL,
+	ServerUrl: isDev ? DEV_SERVER_URL : DEFAULT_SERVER_URL,
 };
