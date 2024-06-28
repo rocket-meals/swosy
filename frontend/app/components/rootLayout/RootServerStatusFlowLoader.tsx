afbda9b6b59a7c9f39f73dd1a76ff2594893a92b
Justin Wittwer <justin.wittwer@gmx.de>
2024-06-28 22:50:09 +0200

    Successfully patched RootServerStatusFlowLoader.tsx

--- frontend/app/components/rootLayout/RootServerStatusFlowLoader.tsx
+++ frontend/app/components/rootLayout/RootServerStatusFlowLoader.tsx
@@ -39,9 +39,8 @@ export const RootServerStatusFlowLoader = (props: ServerStatusFlowLoaderProps) =
 	const nowAsKey = nowInMs.toString();
 	const [continueWithCache, setContinueWithCache] = useState<boolean>(false);
 
-	const [serverInfo, setServerInfo, serverInfoRaw, setServerInfoRaw
-	] = useServerInfoRaw();
-	const [authData, setAuthData] = useSyncState<AuthenticationData>(PersistentSecureStore.authentificationData)
+	const [serverInfo, setServerInfo, serverInfoRaw, setServerInfoRaw] = useServerInfoRaw();
+	const [authData, setAuthData] = useSyncState<AuthenticationData, AuthenticationData>(PersistentSecureStore.authentificationData)
 
 	// TODO: move this to a helper function
 	ServerAPI.createAuthentificationStorage(async () => {
