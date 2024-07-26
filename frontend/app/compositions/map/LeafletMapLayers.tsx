08287d87251b293f1f7066ceb1ee9f03708b3a3f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2024-07-26 17:35:59 +0200

    LeafletMapLayers.tsx aktualisieren

--- frontend/app/compositions/map/LeafletMapLayers.tsx
+++ frontend/app/compositions/map/LeafletMapLayers.tsx
@@ -28,7 +28,7 @@ static StamenToner: MapLayer = {
     baseLayerIsChecked: true,
     layerType: "TileLayer",
     baseLayer: true,
-    url: "https://tiles-eu.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
+    url: "https://tiles-eu.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png",
     attribution: "&copy; <a href='http://maps.stamen.com/'>Stamen Design</a>, <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a>, &copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
 };
 
