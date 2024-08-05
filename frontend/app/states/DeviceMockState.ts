18f398248405ae52fd9aa2cd05ab424681fa0808
Nils <nilsbaumgartner1994@gmail.com>
2024-08-06 01:07:07 +0200

    Device Mock State Show always when set param

--- frontend/app/states/DeviceMockState.ts
+++ frontend/app/states/DeviceMockState.ts
@@ -2,18 +2,22 @@ import {useGlobalSearchParams} from "expo-router";
 
 export type DeviceMockType = undefined | 'none' | 'iphone' | 'android'
 
+class DeviceMockSessionCache {
+	public static DEVICE_MOCK_TYPE: DeviceMockType | undefined = undefined
+}
+
 export function useDeviceMockTypeState(): DeviceMockType {
 	const globalSearchParams = useGlobalSearchParams()
 	const valueRaw = globalSearchParams?.deviceMock
-	if (valueRaw === undefined) {
-		return undefined
-	}
+
 	// check is valueRaw is a string or an array
 	if (typeof valueRaw === 'string') {
 		if (valueRaw === 'none' || valueRaw === 'iphone' || valueRaw === 'android') {
-			return valueRaw
+			// set the value to the session cache
+			DeviceMockSessionCache.DEVICE_MOCK_TYPE = valueRaw
 		}
 	}
-	// if it is an array, return undefined
-	return undefined
+
+	// return the value from the session cache
+	return DeviceMockSessionCache.DEVICE_MOCK_TYPE
 }
