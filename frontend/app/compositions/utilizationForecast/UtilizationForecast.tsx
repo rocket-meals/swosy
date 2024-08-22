8796991dc9b5e3cb03a0384cd804f632c27fa165
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2024-08-22 23:28:22 +0200

    UtilizationForecast.tsx aktualisieren

--- frontend/app/compositions/utilizationForecast/UtilizationForecast.tsx
+++ frontend/app/compositions/utilizationForecast/UtilizationForecast.tsx
@@ -107,7 +107,7 @@ export const UtilizationForecast = (props: UtilizationForecastProps) => {
 	}
 
 	function renderDebug() {
-		if(isDebug || true) {
+		if(isDebug) {
 			return <ScrollView style={{
 				width: '100%',
 				height: 400
