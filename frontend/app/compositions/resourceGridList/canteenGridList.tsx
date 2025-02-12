e45917ba3a9809979fa1d78e7a79c2c8259ae8bc
Nils <nilsbaumgartner1994@gmail.com>
2025-02-12 23:44:56 +0100

    Frontend: Sort Canteens

--- frontend/app/compositions/resourceGridList/canteenGridList.tsx
+++ frontend/app/compositions/resourceGridList/canteenGridList.tsx
@@ -26,6 +26,52 @@ export const getCanteenName = (canteen: Canteens | null | undefined) => {
 	return canteen.alias || canteen.id+''
 }
 
+export const useSortedCanteens = () => {
+	const [canteenDict, setCanteenDict] = useSynchedCanteensDict();
+	const canteen_keys = Object.keys(canteenDict || {});
+	canteen_keys.sort((a, b) => {
+		const canteen_a = canteenDict[a];
+		const canteen_b = canteenDict[b];
+		if (!canteen_a || !canteen_b) {
+			return 0;
+		}
+		let canteenAShouldBeFirst = -1;
+		let canteenBShouldBeFirst = 1;
+
+		// if status is archived, it should be last
+		if (canteen_a.status === ItemStatus.ARCHIVED && canteen_b.status !== ItemStatus.ARCHIVED) {
+			return canteenBShouldBeFirst;
+		}
+		if (canteen_b.status === ItemStatus.ARCHIVED && canteen_a.status !== ItemStatus.ARCHIVED) {
+			return canteenAShouldBeFirst;
+		}
+
+		// otherwise, sort by sort order
+		let canteen_a_sort = canteen_a.sort;
+		let canteen_b_sort = canteen_b.sort;
+
+		if (canteen_a_sort === canteen_b_sort) {
+			return 0;
+		}
+		if(canteen_a_sort === undefined || canteen_a_sort === null){
+			return canteenBShouldBeFirst;
+		}
+		if(canteen_b_sort === undefined || canteen_b_sort === null){
+			return canteenAShouldBeFirst;
+		}
+		if(canteen_a_sort < canteen_b_sort){
+			return canteenAShouldBeFirst;
+		} else if (canteen_a_sort > canteen_b_sort){
+			return canteenBShouldBeFirst;
+		} else {
+			return 0;
+		}
+
+
+	});
+	return canteen_keys.map((key) => canteenDict[key]);
+}
+
 export const CanteenGridList: FunctionComponent<AppState> = ({onPress, ...props}) => {
 	const [canteenDict, setCanteenDict] = useSynchedCanteensDict();
 	const [buildingsDict, setBuildingsDict] = useSynchedBuildingsDict()
@@ -41,11 +87,12 @@ export const CanteenGridList: FunctionComponent<AppState> = ({onPress, ...props}
 
     type DataItem = { key: string; data: Canteens }
     const data: DataItem[] = []
+
+	let sortedCanteens = useSortedCanteens()
     if (canteenDict) {
-    	const canteen_keys = Object.keys(canteenDict);
-    	for (let i=0; i<canteen_keys.length; i++) {
-    		const canteen_key = canteen_keys[i];
-    		const canteen = canteenDict[canteen_key]
+    	for (let i = 0; i < sortedCanteens.length; i++) {
+    		const canteen = sortedCanteens[i];
+			const canteen_key = canteen.id + ''
     		if(!!canteen) {
 				let addCanteen = true
 				if (!showArchived && canteen.status === ItemStatus.ARCHIVED) {
