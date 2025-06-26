d8c1b69c8110961807aacd88a52980712bb630f8
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-06-27 01:56:46 +0200

    fix bottom sheet visibility

--- frontend/app/components/BaseBottomSheet/styles.ts
+++ frontend/app/components/BaseBottomSheet/styles.ts
@@ -1,6 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 50,
+  },
   header: {
     width: '100%',
     flexDirection: 'row',
