e866c44cbce974cd3bf439a8f82456f63530a1aa
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-12-10 12:10:04 +0100

    Simplify debug view components

--- apps/frontend/app/app/(app)/settings/index.tsx
+++ apps/frontend/app/app/(app)/settings/index.tsx
@@ -350,19 +350,22 @@ const Settings = () => {
 					</TouchableOpacity>
 					{isManagement && isDevMode && <Text style={{ ...styles.devModeText, color: theme.screen.text }}>{translate(TranslationKeys.developerModeActive)}</Text>}
                                         {isManagement && isDevMode && (
-                                                <DebugView
-                                                        title={translate(TranslationKeys.debug_mode)}
-                                                        logs={[
-                                                                `${translate(TranslationKeys.backend_server)}: ${
-                                                                        serverInfo?.info?.project?.project_name || '-'
-                                                                }`,
-                                                                `${translate(TranslationKeys.debug_mode)}: ${
-                                                                        debugMode
-                                                                                ? translate(TranslationKeys.checked)
-                                                                                : translate(TranslationKeys.unchecked)
-                                                                }`,
-                                                        ]}
-                                                >
+                                                <DebugView>
+                                                        <View style={{ gap: 4 }}>
+                                                                <Text style={{ color: theme.screen.text }}>
+                                                                        {`${translate(TranslationKeys.backend_server)}: ${
+                                                                                serverInfo?.info?.project?.project_name || '-'
+                                                                        }`}
+                                                                </Text>
+                                                                <Text style={{ color: theme.screen.text }}>
+                                                                        {`${translate(TranslationKeys.debug_mode)}: ${
+                                                                                debugMode
+                                                                                        ? translate(TranslationKeys.checked)
+                                                                                        : translate(TranslationKeys.unchecked)
+                                                                        }`}
+                                                                </Text>
+                                                        </View>
+
                                                         <View style={{ gap: 0 }}>
                                                                 <SettingsList iconBgColor={primaryColor} leftIcon={<MaterialCommunityIcons name="server" size={24} color={theme.screen.icon} />} label={translate(TranslationKeys.backend_server)} value={serverInfo?.info?.project?.project_name} rightIcon={<Octicons name="chevron-right" size={24} color={theme.screen.icon} />} handleFunction={openServerSheet} groupPosition="top" />
                                                                 <SettingsList iconBgColor={primaryColor} leftIcon={<MaterialCommunityIcons name="clock-outline" size={24} color={theme.screen.icon} />} label={translate(TranslationKeys.foodoffers_next_day_time)} value={(foodOffersNextDayThreshold || '18:00').toString()} rightIcon={<Octicons name="chevron-right" size={24} color={theme.screen.icon} />} handleFunction={openFoodOffersTimeSheet} groupPosition="middle" />
