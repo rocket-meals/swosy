cf59f9be0b0c8ebc87eb71185d50798ac63e45ba
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-12-21 19:20:00 +0100

    Remove border radius from language sheet top

--- apps/frontend/app/components/LanguageSheet/styles.ts
+++ apps/frontend/app/components/LanguageSheet/styles.ts
@@ -1,35 +1,29 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-	sheetView: {
-		width: '100%',
-		height: '100%',
-		borderTopRightRadius: 28,
-		borderTopLeftRadius: 28,
-		padding: 10,
-		paddingBottom: 0,
-	},
+        sheetView: {
+                width: '100%',
+                height: '100%',
+                padding: 0,
+        },
 	contentContainer: {
 		alignItems: 'center',
 	},
 	sheetHeader: {
 		width: '100%',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderTopRightRadius: 28,
-		borderTopLeftRadius: 28,
-	},
-	sheetHeading: {
-		fontFamily: 'Poppins_700Bold',
-	},
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+        },
+        sheetHeading: {
+                fontFamily: 'Poppins_700Bold',
+        },
         optionsContainer: {
                 width: '100%',
-                paddingHorizontal: 10,
-                marginTop: 20,
+                marginTop: 0,
         },
         flagIcon: {
-                width: 35.2,
-                height: 22,
+                width: 32,
+                height: 32,
         },
 });
