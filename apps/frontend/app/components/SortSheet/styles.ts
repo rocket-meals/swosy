3da64a232019713622141ae874b3a56e7fed0405
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-12-23 21:20:28 +0100

    Reduce sort sheet spacing

--- apps/frontend/app/components/SortSheet/styles.ts
+++ apps/frontend/app/components/SortSheet/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-        sortingListContainer: {
-                width: '100%',
-                paddingHorizontal: 10,
-                marginTop: 30,
-        },
+	sortingListContainer: {
+		width: '100%',
+		paddingHorizontal: 10,
+		marginTop: 12,
+	},
 });
