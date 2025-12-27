d4b1538d414361a26e08c64813f9ce5cdb58f76f
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-12-27 18:53:51 +0100

    Inline ProjectButton types

--- apps/frontend/app/components/ProjectButton/index.tsx
+++ apps/frontend/app/components/ProjectButton/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Appearance, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { ReactNode } from 'react';
+import { Appearance, StyleProp, StyleSheet, Text, TouchableOpacity, ViewStyle } from 'react-native';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/reducer';
 import { myContrastColor } from '@/helper/ColorHelper';
 import { darkTheme, lightTheme } from '@/styles/themes';
-import { ProjectButtonProps } from './types';
+
+export interface ProjectButtonProps {
+	text: string;
+	onPress?: () => void;
+	iconLeft?: ReactNode;
+	iconRight?: ReactNode;
+	backgroundColor?: string;
+	style?: StyleProp<ViewStyle>;
+}
 
 const ProjectButton: React.FC<ProjectButtonProps> = ({ text, onPress, iconLeft, iconRight, backgroundColor, style }) => {
 	const { primaryColor, selectedTheme } = useSelector((state: RootState) => state.settings);
