4da560f5f2c7c1abff4d071835425b117efb6311
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2026-01-01 00:19:32 +0100

    Add spacing before cancel action in image edit modal

--- apps/frontend/app/constants/Constants.ts
+++ apps/frontend/app/constants/Constants.ts
@@ -3,6 +3,7 @@ import { Platform } from 'react-native';
 export const isWeb = Platform.OS === 'web';
 export const borderRadiusContainer = 10;
 export const horizontalScreenPadding = 16;
+export const settingsListSectionGap = 8;
 
 export const links = [
 	{ title: 'about_us', path: '/about-us' },
