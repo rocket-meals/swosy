ac40baa83ea2b76b61b88291e8935ac4dd7b8da5
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2025-12-31 08:58:08 +0100

    Inline BuildingItem styles

--- apps/frontend/app/components/BuildingItem/BuildingItem.tsx
+++ apps/frontend/app/components/BuildingItem/BuildingItem.tsx
@@ -5,7 +5,6 @@ import { isWeb } from '@/constants/Constants';
 import { excerpt, getImageUrl } from '@/constants/HelperFunctions';
 import { useTheme } from '@/hooks/useTheme';
 import { myContrastColor } from '@/helper/ColorHelper';
-import styles from './styles';
 import { router } from 'expo-router';
 import { getDistanceUnit } from '@/helper/distanceHelper';
 import { Tooltip, TooltipContent, TooltipText } from '@gluestack-ui/themed';
@@ -191,7 +190,63 @@ function areEqual(prev: BuildingItemPropsOptimized, next: BuildingItemPropsOptim
 
 export default memo(BuildingItem, areEqual);
 
-const localStyles = StyleSheet.create({
+const styles = StyleSheet.create({
+	overlay: {
+		width: '100%',
+		height: '100%',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		backgroundColor: 'rgba(0,0,0,0.2)',
+		borderTopRightRadius: 18,
+		borderTopLeftRadius: 18,
+	},
+	imageActionContainer: {
+		width: '100%',
+		position: 'absolute',
+		bottom: 0,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	editImageButton: {
+		width: 35,
+		height: 35,
+		borderRadius: 50,
+		backgroundColor: 'rgba(0,0,0,0.5)',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	navigationButton: {
+		position: 'absolute',
+		top: 10,
+		right: 10,
+		width: 36,
+		height: 36,
+		borderRadius: 18,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	directionButton: {
+		borderRadius: 8,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 10,
+		paddingVertical: 5,
+		paddingHorizontal: 10,
+	},
+	distance: {
+		fontSize: 16,
+		fontFamily: 'Poppins_400Regular',
+	},
+	campusName: {
+		fontSize: 16,
+		fontFamily: 'Poppins_400Regular',
+		textAlign: 'center',
+		marginBottom: 5,
+	},
+	dummy: {},
 	placeholder: {
 		width: 1,
 		height: 1,
