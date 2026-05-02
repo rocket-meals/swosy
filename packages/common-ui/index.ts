d125d9a45124dacd7dfb39a66173887948cf1186
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2026-05-02 07:13:32 +0200

    Revert "Re-add MyAvatar with local SVG generation via @dicebear/core + react-native-svg"

--- packages/common-ui/index.ts
+++ packages/common-ui/index.ts
@@ -93,7 +93,3 @@ export type { CardWithTextProps } from './src/components/CardWithText';
 
 export { default as SettingsListMyMapThemeSelection } from './src/components/SettingsListMyMapThemeSelection';
 export type { SettingsListMyMapThemeSelectionProps } from './src/components/SettingsListMyMapThemeSelection';
-
-export { default as MyAvatar } from './src/components/MyAvatar';
-export { AvatarStyle } from './src/components/MyAvatar/types';
-export type { MyAvatarProps } from './src/components/MyAvatar/types';
