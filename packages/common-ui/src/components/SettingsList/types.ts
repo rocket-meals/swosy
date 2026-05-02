c1f1be033aaa250baafee65e9d7569c7293023d0
Ahmed Fazil <aliyanansari321@gmail.com>
2026-05-02 16:08:42 +0500

    Add reverseLayout and titleTextAlign props

--- packages/common-ui/src/components/SettingsList/types.ts
+++ packages/common-ui/src/components/SettingsList/types.ts
@@ -75,6 +75,14 @@ type SettingsListPropsOwn = SettingsListItemBaseProps & {
 	 * true. Consuming apps can use this to open an account-required modal.
 	 */
 	onAccountRequired?: () => void;
+	/**
+	 * When true, the layout of the item is reversed (RTL support).
+	 */
+	reverseLayout?: boolean;
+	/**
+	 * Text alignment for the title.
+	 */
+	titleTextAlign?: 'left' | 'center' | 'right' | 'justify' | 'auto';
 };
 
 export type SettingsListProps = PropsWithChildren<SettingsListPropsOwn>;
